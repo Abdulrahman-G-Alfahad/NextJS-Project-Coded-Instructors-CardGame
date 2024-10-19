29c517424be7908ce48a4fc5123d918e486a13f1
unknown <abdulrahman.g.alfahad@gmail.com>
2024-10-19 11:13:54 +0300

    Added a reset button that resets the game and score

--- src/app/Components/ResultTracker.js
+++ src/app/Components/ResultTracker.js
@@ -4,24 +4,38 @@ import CardsList from "./CardsList";
 
 function ResultTracker({ instructors }) {
   const [score, setScore] = useState(0);
-  const [faild, setFaild] = useState(0);
+  const [failed, setFailed] = useState(0);
+  const [resetKey, setResetKey] = useState(0);
 
   const incrementScore = () => {
-    setScore((newScore) => newScore + 1);
+    setScore((prevScore) => prevScore + 1);
   };
 
-  const decrementScore = () => {
-    setFaild((newScore) => newScore + 1);
+  const incrementFailed = () => {
+    setFailed((prevFailed) => prevFailed + 1);
+  };
+
+  const resetGame = () => {
+    setScore(0);
+    setFailed(0);
+    setResetKey((prevKey) => prevKey + 1);
   };
 
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Score: {score}</h2>
-      <h2 className="text-2xl font-bold mb-4">Faild Attampts: {faild}</h2>
+      <h2 className="text-2xl font-bold mb-4">Failed Attempts: {failed}</h2>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={resetGame}
+      >
+        Reset Game
+      </button>
       <CardsList
+        key={resetKey}
         instructors={instructors}
         incrementScore={incrementScore}
-        decrementScore={decrementScore}
+        incrementFailed={incrementFailed}
       />
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import instructors from "./Data/Instructors";
 import ResultTracker from "./Components/ResultTracker";
 
 export default function Home() {
-  console.log("Instructors = ", instructors);
   return (
     <div className="">
       <h1 className="leading-relaxed font-primary font-extrabold text-4xl text-center text-palette-primary mt-4 py-2 sm:py-4">
